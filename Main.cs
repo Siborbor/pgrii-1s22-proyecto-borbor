97499d853cbe9c1e3b4a401ebc620dd277488bea
Steven Borbor <steven.borbor@gmail.com>
2022-07-27 00:15:54 -0500

    Creacion de clase tarea
    Cree la clase Tareas con su repectivo constructor, setter y getter, hice pruebas de instanciamiento de la clase

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using CrearTarea;
+
+DateTime thisDate1 = new DateTime(2022, 7, 22);
+DateTime thisDate2 = new DateTime(2022, 7, 26);
+Tarea t1 = new Tarea();
+
+t1.ID = 12345678;
+t1.Nombre = "Tarea de Matematica";
+t1.Description = "esta es una tarea de prueba";
+t1.FechaCreacion = thisDate1.ToString("dd-MM-yyyy");
+t1.FechaLimiteTarea = thisDate2.ToString("dd-MM-yyyy");
+t1.Estado = "CREADA";
+
+Console.WriteLine($"ID: {t1.ID}" );
+Console.WriteLine($"NOMBRE: {t1.Nombre}");
+Console.WriteLine($"DESCRIPCION: {t1.Description}");
+Console.WriteLine($"FECHA DE CREACION: {t1.FechaCreacion}");
+Console.WriteLine($"fECHA LIMITE DE TAREA: {t1.FechaLimiteTarea}");
+Console.WriteLine($"ESTADO DE LA TAREA: {t1.Estado}");
---
+++ Tarea.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrearTarea
+{
+    internal class Tarea
+    {
+        private int id;
+        private string nombre;
+        private string description;
+        private string fecha_creacion;
+        private string fecha_limite_tarea;
+        private string estado;
+
+        public Tarea()
+        {
+            id = 0;
+            nombre = "";
+            description = "";
+            fecha_creacion = "";
+            fecha_limite_tarea = "";
+            estado = "";
+        }
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; } 
+            set { nombre = value; }
+        }
+
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string FechaCreacion
+        {
+             get { return fecha_creacion; }
+             set { fecha_creacion = value; }
+        }
+
+        public string FechaLimiteTarea
+        {
+            get { return fecha_limite_tarea; }
+            set { fecha_limite_tarea = value; }
+        }
+
+        public string Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+    }
+}
+
